22b978bd1f6c88cec945e10635e07040af49ca12
jenniferarce <arcej95@hotmail.com>
2017-03-21 12:33:19 -0300

    Routing excercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {TableComponent} from './table-test/table.component';
-import{HttpModule, JsonpModule} from '@angular/http';
-import{GitHubComponent} from './github-list/github.component';
 
-//import{ZippyComponent} from './zippy/zippy.component';
-//import{SubscribeComponent} from './subscribe-form/subscribe-form.component';
-
-//import{SignUpFormComponent} from './signup-form/signup-form.component';
-
-//import{PasswordComponent} from './password-form/password-form.component';
-
-//import { TweetsComponent } from './tweets/tweets.component';
-//import {SummaryPipe} from './summary-pipe/summary.pipe';
-//import{BootstrapPanel} from './ng-content-example/bootstrap.panel.component';
-
-//import{ContactFormComponent} from './contact-form/contact-form.component';
+//routers
+import { HomeComponent } from './routers/home.component';
+import { NotFoundComponent } from './routers/not-found.component';
+import { MessagesModule } from './routers/messages/messages.module'; 
+import { PhotosModule } from './routers/photos/photos.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpModule, JsonpModule],
-  declarations: [AppComponent,TableComponent,GitHubComponent],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule,MessagesModule,PhotosModule],
+  declarations: [AppComponent,HomeComponent,NotFoundComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
 
-// TweetsComponent, SummaryPipe,BootstrapPanel,ContactFormComponent,
-//ZippyComponent,SubscribeComponent,
-//SignUpFormComponent, PasswordComponent
+export class AppModule { 
+
+}
+
