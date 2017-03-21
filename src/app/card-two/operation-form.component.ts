7be434336680f5d1ee6a0a8fc651b60b32aaabb4
jenniferarce <arcej95@hotmail.com>
2017-03-21 17:01:12 -0300

    card 2 - operations (incomplete)

---
+++ src/app/card-two/operation-form.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'operation-form',
+    templateUrl: 'app/card-two/operation-form.html'
+})
+
+export class OperationComponent{
+
+}
