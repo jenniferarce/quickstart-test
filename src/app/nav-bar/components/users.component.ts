9965a4338d32db3ed4c14496be6aca094fe04af5
jenniferarce <arcej95@hotmail.com>
2017-03-23 12:35:58 -0300

    spin icon

--- src/app/nav-bar/components/users.component.ts
+++ src/app/nav-bar/components/users.component.ts
@@ -5,25 +5,32 @@ import { UsersService } from '../services/users.service';
 
 @Component({
     template: `<h4>Users</h4>
+    <div *ngIf="isLoading">
+            <i class="fa fa-spinner fa-spin fa-3x"></i>
+        </div>
+    <div>
+        <button class="btn btn-primary">Add User</button>
 
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>E-mail</th>
-                <th>Edit</th>
-                <th>Delete</th>
-            </tr>
-        </thead>
-        <tbody *ngFor="let user of users">
-            <tr>
-                <td>{{user.name}}</td>
-                <td>{{user.email}}</td>
-                <td><i class="glyphicon glyphicon-edit"></i></td>
-                <td><i class="glyphicon glyphicon-remove"></i></td>
-            </tr>
-        </tbody>
-    </table>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>E-mail</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody *ngFor="let user of users">
+                <tr>
+                    <td>{{user.name}}</td>
+                    <td>{{user.email}}</td>
+                    <td><i class="glyphicon glyphicon-edit"></i></td>
+                    <td><i class="glyphicon glyphicon-remove"></i></td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
     `,
     providers: [UsersService]
 })
@@ -36,10 +43,9 @@ export class UsersComponent implements OnInit {
     }
 
     ngOnInit(){
-        this._usersService.getUsers()
-                                      .subscribe(user=>{this.users = user},
-                                        ()=>{this.isLoading=false;} );
-
-    console.log(this.users);
+        this._usersService.getUsers().subscribe(
+                                    user=>{this.users = user; 
+                                    this.isLoading=false;
+                                        });
     }
 }
